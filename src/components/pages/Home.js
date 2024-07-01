59c3a5a435f1f42d8f952864ede8928524aadcbf
yousseflets <leticiayoussef.si@gmail.com>
2024-07-01 16:43:55 -0300

    correcoes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,5 @@
 import styles from './Home.module.css'
 import home from "../../img/lotusSpa2.png";
-import divider from "../../img/divider.png";
-import Company from './Company';
 import Services from './Services';
 
 
@@ -9,10 +7,9 @@ function Home() {
   return (
     <section className={styles.home_container}>
       <img src={home} alt="Lotus" />
-      <Company />
-      <img src={divider} alt="Lotus" height={100}/>
+      {/* <Company />
+      <img src={divider} alt="Lotus" height={100}/> */}
       <Services />
-      <img src={divider} alt="Lotus" height={100}/>
     </section>
   
   )
